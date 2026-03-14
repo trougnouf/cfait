6c04e372339dc1788c69565c137f7692a9144eb9
Benoit Brummer (Trougnouf) <trougnouf@disroot.org>
2026-03-14 12:12:26 +0100

    fix(Android): compilation issue / missing import

--- android/app/src/main/java/com/trougnouf/cfait/ui/HelpScreen.kt
+++ android/app/src/main/java/com/trougnouf/cfait/ui/HelpScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
